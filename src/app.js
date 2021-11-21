42a9282ebdf8998bf5a106ae6db687f7965bfcce
Flavia Lisboa <lisboafla16@gmail.com>
2021-11-21 22:56:10 +0100

    update time

--- src/app.js
+++ src/app.js
@@ -63,10 +63,7 @@ function searchLocation(position) {
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
+
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
@@ -88,6 +85,23 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
+function formatDate (timestamp) {
+
+  let date = new Date (timestamp);
+  let hours = date.getHours();
+   if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+   if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day =  days[date.getDay()];
+
+   return `${day} ${hours}:${minutes}`;
+
+  }
 
 
 let dateElement = document.querySelector("#date");
