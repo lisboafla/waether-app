6dc23ed25e109d3705756755c4e64b89ff50ff28
Flavia Lisboa <lisboafla16@gmail.com>
2021-11-21 20:23:14 +0100

    added icon JS

--- src/app.js
+++ src/app.js
@@ -35,6 +35,13 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+      iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
 }
 
 function searchCity(city) {
@@ -82,5 +89,7 @@ searchForm.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let iconElement = document.querySelector("#icon");
 
 searchCity("Rio de Janeiro");
