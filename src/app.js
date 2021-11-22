d3cdbce15a1a70c3c734ea457952ab69b57aba93
Flavia Lisboa <lisboafla16@gmail.com>
2021-11-22 18:29:28 +0100

    updated js

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ iconElement.setAttribute(
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
-
+ celsiusTemperature = response.data.main.temp;
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
